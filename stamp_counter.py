d8a9d7aa8b6575ffc2cdd1c9630ae8454c8202f6
NITISH KUMAR <55353260+nitish0308@users.noreply.github.com>
2026-04-26 23:52:35 +0530

    Update stamp_counter.py

--- stamp_counter.py
+++ stamp_counter.py
@@ -1,3 +1,6 @@
+#Rupal has a huge collection of country stamps. She decided to count the total number of distinct country stamps in her collection. She asked for your help. You pick the stamps one by one from a stack of N country stamps.
+
+
 no_country=5
 stamp=['UK', 'China', 'USA', 'France', 'New Zealand', 'UK', 'France']
 
